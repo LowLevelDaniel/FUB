220221fd76b7386276aecbbfdb5d2e1692d78330
Daniel Alder <ubuntu@server2>
2024-10-03 22:13:01 +0000

    I have spent over 1 day on an error which only existed because apparently the -nographic option doesn't run a normal system but using terminal ui it actually removes video drivers from the machine being emulated, meaning printing in protected isn't actually an option, I Love QEMU

--- x86/run.sh
+++ x86/run.sh
@@ -2,15 +2,15 @@
 
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
-  qemu-system-x86_64 -drive format=raw,file=boot.img -nographic -m 4096 
+  qemu-system-x86_64 -drive format=raw,file=boot.img -m 4096 
   exit 0
 fi
 
 # Check if the argument is equal to 32
 if [ "$1" -eq 32 ]; then
-  qemu-system-i386 -fda boot.img -nographic
-  # qemu-system-i386 -drive format=raw,file=boot.img -nographic
+  qemu-system-i386 -fda boot.img
+  # qemu-system-i386 -drive format=raw,file=boot.img
 else
-  qemu-system-x86_64 -drive format=raw,file=boot.img -nographic -m 4096 
+  qemu-system-x86_64 -drive format=raw,file=boot.img -m 4096 
 fi
 exit 0
